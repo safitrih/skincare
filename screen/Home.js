c5f911481e91d9a987d43ca81eae2033815d8837
mafruha86 <mafmafruhah85@gmail.com>
2023-11-09 11:49:52 +0700

    mengubah halaman produk

--- screen/Home.js
+++ screen/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'; // Anda dapat mengganti "FontAwesome" dengan ikon yang Anda inginkan.
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Home = ({ navigation }) => {
   return (
     <View>
       <Icon name="home" size={50} /> {/* Ganti "home" dengan ikon yang sesuai */}
       <Text>Welcome to Skincer Shop!</Text>
-      {/* Tambahkan konten lain di sini */}
+      {}
       <Button title="Go to Products" onPress={() => navigation.navigate('Product')} />
     </View>
   );
